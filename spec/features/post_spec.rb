60f0d2fecdc3fc71bb0c5b525b1392a7afecda2c
shoiraTa <shoira.shakirovna@gmail.com>
2022-08-08 17:43:52 +0400

    feat: index action Posts

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts' do 
+  describe 'index' do
+    it 'can be reached successfully' do
+      visit posts_path
+      expect(page.status_code).to eq(200) 
+    end
+    it 'has a title of Posts' do
+      visit posts_path
+      expect(page).to have_content(/Posts/) 
+    end
+  end
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+    it 'can be created' do
+      post = Post.create(rationale: 'hello', date: Date.today)
+      expect(post).to be_valid
+    end
+    it 'cannot be created without date and rationale' do
+      post = Post.create()
+      expect(post).to_not be_valid
+    end
+  end
+
 end
