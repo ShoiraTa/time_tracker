6b1478dd9ad41a59ea912d208d67680546b7e2ce
shoiraTa <shoira.shakirovna@gmail.com>
2022-08-12 21:24:52 +0400

    feat: posts mdoel

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,27 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
   def index
     @posts = Post.all
+  end
+  def new
+    @post = Post.new
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    if @post.save
+      redirect_to @post
+    end
+  end
+
+  private
+  def post_params 
+    params.require(:post).permit(:date, :rationale)
+  end
+
+  def set_post
+    @post = Post.new(post_params)
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe 'Posts' do
       expect(page).to have_content(/Posts/) 
     end
   end
+  describe 'creation' do
+    it 'has a form that can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200) 
+    end
+    it 'can be created from new page' do
+      visit new_post_path
+      fill_in "post[date]",	with: Date.today 
+      fill_in "post[rationale]",	with: 'hello'
+      click_on "Save"
+      expect(page).to have_content('hello')
+    end
+  end
 end
