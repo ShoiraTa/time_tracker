9ec2255b2c732cde41d470f9822c8b360a517673
shoiraTa <shoira.shakirovna@gmail.com>
2022-08-15 18:27:42 +0400

    feat: administrate gem

--- Gemfile
+++ Gemfile
@@ -24,7 +24,8 @@ gem "jquery-rails"
 gem 'bootstrap', '~> 5.1.3'
 gem "sassc-rails" 
 gem 'gritter', '~> 1.2'
-
+gem 'administrate'
+gem 'bourbon'
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :posts
+      resources :users
+      resources :admin_users
+
+      root to: "posts#index"
+    end
   resources :posts
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
