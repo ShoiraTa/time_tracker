10cad290f799836d6037410b05c5881013713861
shoiraTa <shoira.shakirovna@gmail.com>
2022-08-16 18:54:02 +0400

    feat: add permitionsr

--- spec/features/approving_posts_spec.rb
+++ spec/features/approving_posts_spec.rb
@@ -9,12 +9,19 @@ RSpec.describe 'Approving posts' do
     before do 
       @post = create(:post)
     end
-    it 'can select' do
+    it 'admin can select approve' do
       visit edit_post_path(@post)
-       choose('post_status_approved')
-       click_on 'Save'
-       @post.reload
-       expect(@post.status).to eq('approved') 
+        choose('post_status_approved')
+        click_on 'Save'
+        @post.reload
+        expect(@post.status).to eq('approved') 
+    end
+    it 'non-admin cannnot select approve' do
+        logout(:user)
+        @user = create(:user)
+        login_as(@user, :scope => :user)
+        visit edit_post_path(@post)
+        expect(page).not_to have_content('Approved') 
     end
   end
 end
