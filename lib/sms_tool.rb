e3358520e25253d8b0a33f3f7b9d72bde5f24d71
shoiraTa <shoira.shakirovna@gmail.com>
2022-08-18 11:29:12 +0400

    feat: added life connection with twillio to send sms

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,6 +1,16 @@
 module SmsTool
+  account_sid= ENV['TWILLIO_ACCOUNT_SID']
+  auth_token= ENV['TWILLIO_AUTH_TOKEN']
+
+  @client = Twilio::REST::Client.new(account_sid, auth_token)
+
   def self.send_sms(number:, message:) 
-    puts "sending sms #{message}"
-    puts "to #{number}"
+    @client.messages.create(
+      from: ENV['TWILLIO_PHONE_NUMBER'],
+      to: "+#{number}",
+      body: "#{message}"
+    )
   end
 end
+
+# SmsTool.send_sms(number: '995551113368', message: 'Hello from overtime app')
