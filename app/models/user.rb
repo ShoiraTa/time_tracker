ddaa3aa10b08cc6ca2bfa6ddfa90b075086a19e3
shoiraTa <shoira.shakirovna@gmail.com>
2022-08-18 11:09:50 +0400

    feat: add phone number validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  PHONE_NUMBER_REGEX = /\A[0-9]*\Z/
+  validates_format_of :phone_number, with: PHONE_NUMBER_REGEX
+  validates :phone_number, length: {minimum: 10}
 
   validates_presence_of :first_name, :last_name, :phone_number
+
   has_many :posts
 
   def full_name
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,17 @@ RSpec.describe User, type: :model do
 
   let(:user){User.create(email:'email@gmail.com', password: '123456', password_confirmation: '123456', last_name: 'Last',first_name: 'Nina', phone_number: '995551113368')}
 
-  describe 'Create' do
+  describe 'create' do
     it 'can create user' do
       expect(user).to be_valid
     end
+  end
+  describe 'methods' do
+    it 'full_name method' do 
+      expect(user.full_name).to eq('NINA, LAST')
+    end
+  end
+  describe 'validation' do
     it 'can not create user without first_name last_name' do
       user.first_name = nil 
       user.last_name = nil 
@@ -17,10 +24,13 @@ RSpec.describe User, type: :model do
       user.phone_number = nil
       expect(user).to_not be_valid
     end
-  end
-  describe 'user methods' do
-    it 'full_name method' do 
-      expect(user.full_name).to eq('NINA, LAST')
+    it 'requires phone attr to only contain numbers' do
+      user.phone_number = '1-2-3-4-5'
+      expect(user).to_not be_valid
+    end
+    it 'requires phone attr to only contain 10 chars' do
+      user.phone_number = '123456789'
+      expect(user).to_not be_valid
     end
   end
 end
