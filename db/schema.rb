ec2ebea2fd2c65aeb8bfd8c370002522aed2ff61
shoiraTa <shoira.shakirovna@gmail.com>
2022-08-21 19:21:49 +0400

    feat: created self-referencial model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_18_073726) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_21_152007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_073726) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_auditlogs_on_user_id"
+  end
+
+  create_table "hands", force: :cascade do |t|
+    t.integer "hand_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
